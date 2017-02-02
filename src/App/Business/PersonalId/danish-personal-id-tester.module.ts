0e133a9adf4d4e1eafc3596171598552ccbb72be
Jan Schrøder Hansen <jan.sch.hansen@gmail.com>
2017-02-02 19:40:34 +0100

    backup

--- src/App/Business/PersonalId/danish-personal-id-tester.module.ts
+++ src/App/Business/PersonalId/danish-personal-id-tester.module.ts
@@ -3,6 +3,9 @@ module Janosh.TypeScriptPersonalIdCheckAndGenerator.App.Business.PersonalId {
     export class DanishPersonalIdTester implements PersonalIdTester{
         test(personalId: string) {
             return true;        
+
+
+            
         }
     }
 }
